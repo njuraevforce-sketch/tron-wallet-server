ed8ae723b624f35c4864980355197e474c1125fc
njuraevforce-sketch <n.juraevforce@gmail.com>
2026-05-04 21:05:09 +0500

    Update server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ function simpleRateLimit(req, res, next) {
 app.use(simpleRateLimit);
 
 // ========== CONSTANTS ==========
-const MIN_DEPOSIT = 0;
+const MIN_DEPOSIT = 17;
 
 // BSC
 const USDT_BSC_CONTRACT = '0x55d398326f99059fF775485246999027B3197955';
